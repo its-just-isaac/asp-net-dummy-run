1c46105ef001afecddc825290e8b85e9a1516f4b
mikomori <mikomori0909@gmail.com>
2021-12-24 10:55:04 +0800

    Added integration testing samples

---
+++ src/Apps/03_IntegrationTestingBestPractices/TennisBookings.Merchandise.Api.IntegrationTests/CategoriesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TennisBookings.Merchandise.Api.IntegrationTests
+{
+    public class CategoriesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriesControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            // option 1
+            // _httpClient = factory.CreateDefaultClient(new Uri("http://localhost/api/Categories"));
+
+            // option 2
+            factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/Categories");
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnSuccessStatusCode()
+        {
+            var response = await _httpClient.GetAsync("");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+    }
+}
