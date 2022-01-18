f2e89ec821306643983a629fa18b85c1cf9be09c
unknown <its.just.isaac.0909@gmail.com>
2022-01-19 00:17:19 +0800

    Update project

---
+++ src/Apps/Playground/Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Playground
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var helloWorld = new Action(() => Console.WriteLine("Hello "));
+            helloWorld += () => Console.WriteLine("World ");
+            helloWorld += () => Console.WriteLine("& .Net");
+
+            helloWorld();
+        }
+    }
+}
